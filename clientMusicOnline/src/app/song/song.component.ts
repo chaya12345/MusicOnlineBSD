cf84729c5e758e13b9e3248693ba1852242ad788
bsd <bsd.odaya@gmail.com>
2020-08-28 05:23:01 +0300

    side bsd :)

---
+++ clientMusicOnline/src/app/song/song.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'song',
+  templateUrl: './song.component.html',
+  styleUrls: ['./song.component.css']
+})
+export class SongComponent implements OnInit {
+
+  @Input() song: any;
+  isHover:boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggle_hover(event) :void {
+    this.isHover = !this.isHover;
+    console.log(this.isHover);
+  }
+
+}
