6f999071ade44a8f9cf5dc08e1585865d8508c7f
Hodaya-Azran <0504117455h@gmail.com>
2020-08-30 20:45:24 +0300

    some change

--- ServerMusicBSD/BL/ArtistsNamesBL.cs
+++ ServerMusicBSD/BL/ArtistsNamesBL.cs
@@ -10,7 +10,7 @@ namespace BL
 {
     public class ArtistsNamesBL
     {
-        static MusicOnlineEntities1 et = new MusicOnlineEntities1();
+        static MusicOnlineEntities et = new MusicOnlineEntities();
         public static void AddArtistName(ArtistsNameTBL artistsName)
         {
             et.ArtistsNameTBL.Add(artistsName);
