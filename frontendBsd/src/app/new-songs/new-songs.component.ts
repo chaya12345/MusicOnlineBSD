90e35e9a5ab1b2c0628a1aab5703d4a235dbb5ae
bsd <bsd.odaya@gmail.com>
2020-09-15 20:29:14 +0300

    add swiper

--- frontendBsd/src/app/new-songs/new-songs.component.ts
+++ frontendBsd/src/app/new-songs/new-songs.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
 
 @Component({
   selector: 'new-songs',
   templateUrl: './new-songs.component.html',
-  styleUrls: ['./new-songs.component.css']
+  styleUrls: ['./new-songs.component.css', './swiper-bundle.min.css']
 })
 export class NewSongsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      loop: true,
+      spaceBetween: 10,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
   }
 
 }
