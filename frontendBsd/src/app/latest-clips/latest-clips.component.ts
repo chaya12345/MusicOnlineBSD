609936a537cf43a86af29877c089273ef608ef80
Hodaya-Azran <0504117455h@gmail.com>
2020-09-15 23:08:40 +0300

    last clips

--- frontendBsd/src/app/latest-clips/latest-clips.component.ts
+++ frontendBsd/src/app/latest-clips/latest-clips.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Song } from '../classes/song';
+import { SongService } from '../services/song.service';
 
 @Component({
   selector: 'latest-clips',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LatestClipsComponent implements OnInit {
 
-  constructor() { }
+  clipsList: Song[] = [];
+
+  constructor(private songService: SongService) {
+    try {
+      this.songService.getSongsByTag('קליפ').subscribe(songs => { this.clipsList = songs; this.filter(); }, err => { console.log(err); });
+    }
+    catch { console.log('clips'); }
+  }
 
   ngOnInit(): void {
+  }
+  filter(): void {
+    this.clipsList.sort((a, b) => Math.round(new Date(b.date).getTime() - new Date(a.date).getTime()));
+    this.clipsList = this.clipsList.slice(0, 8);
   }
 
 }
