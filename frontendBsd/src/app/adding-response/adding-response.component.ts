3615cad5c4f36dcedc664f384d096c0fcd45afdf
Hodaya-Azran <0504117455h@gmail.com>
2020-09-23 19:04:48 +0300

    add response to db

--- frontendBsd/src/app/adding-response/adding-response.component.ts
+++ frontendBsd/src/app/adding-response/adding-response.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatAccordion } from '@angular/material';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ResponsesToArticles } from '../classes/responsesToArticles';
+import { ResponsesToSongs } from '../classes/responsesToSongs';
+import { ResponseToSongsService } from '../services/response-to-songs.service';
 import { ResponsesToArticlesService } from '../services/responses-to-articles.service';
 
 @Component({
@@ -15,12 +17,14 @@ export class AddingResponseComponent implements OnInit {
   @ViewChild(MatAccordion, null) accordion: MatAccordion;
   isWantToAdd: boolean = false;
   formRespons: FormGroup;
-  newResponse: ResponsesToArticles = new ResponsesToArticles;
+  newResponsesToArticles: ResponsesToArticles = new ResponsesToArticles;
+  newResponsesToSongs:ResponsesToSongs=new ResponsesToSongs;
 
-  constructor(private responsesToArticlesService: ResponsesToArticlesService, private router: Router) { 
+  constructor(private responsesToArticlesService: ResponsesToArticlesService, private activatedRoute: ActivatedRoute,
+    private responseToSongsService:ResponseToSongsService ) { 
     this.formRespons = new FormGroup({
       fullName: new FormControl("", [Validators.required, Validators.minLength(2)]),
-      // email:new FormControl(""),
+      email:new FormControl("",Validators.email),
       title: new FormControl("", Validators.required),
       message: new FormControl("", Validators.maxLength(100))
     });
@@ -31,19 +35,25 @@ export class AddingResponseComponent implements OnInit {
   
   onSubmit() {
     if (this.formRespons.valid) {
-      var href = this.router.url;
-      var res = href.split("/");
-      this.newResponse.articleId = Number(res[res.length - 1]);
-      this.newResponse.name = this.formRespons.controls.fullName.value;
-      this.newResponse.title = this.formRespons.controls.title.value;
-      this.newResponse.content = this.formRespons.controls.message.value;
-      //השדה מייל מיותר כי אין מייל בתגובות
-      //וגם לא צריך לדעת מה המייל בכלל
-      this.responsesToArticlesService.addResponse(this.newResponse).subscribe();
-      this.reset();
-      this.newResponse = null;
+      if(this.activatedRoute.snapshot.routeConfig.path.includes('song')==true){
+        this.newResponsesToSongs.songId =Number(this.activatedRoute.snapshot.paramMap.get('id'));
+        this.newResponsesToSongs.name = this.formRespons.controls.fullName.value;
+        this.newResponsesToSongs.title = this.formRespons.controls.title.value;
+        this.newResponsesToSongs.content = this.formRespons.controls.message.value;
+        this.responseToSongsService.addResponse(this.newResponsesToSongs).subscribe();
+        this.newResponsesToSongs = null;
+        this.reset();
+      }
+      else if(this.activatedRoute.snapshot.routeConfig.path.includes('article')==true){
+        this.newResponsesToArticles.articleId =Number(this.activatedRoute.snapshot.paramMap.get('id'));
+        this.newResponsesToArticles.name = this.formRespons.controls.fullName.value;
+        this.newResponsesToArticles.title = this.formRespons.controls.title.value;
+        this.newResponsesToArticles.content = this.formRespons.controls.message.value;
+        this.responsesToArticlesService.addResponse(this.newResponsesToArticles).subscribe();
+        this.newResponsesToArticles = null;
+        this.reset();
+      }
     }
-    
   }
   reset() {
     this.formRespons.reset({ value: "" });
