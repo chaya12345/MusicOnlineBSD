423fd3c0b8d892530abc38b595816aa295fcfc48
bsd <bsd.odaya@gmail.com>
2020-09-30 17:28:41 +0300

    update tools and smooth scrolling

--- frontendBsd/src/app/app.component.ts
+++ frontendBsd/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'מיוזיק online | בכל סגנון שתבחר';
+  wheelDistance(evt) {
+
+  // wheelDelta inidicates how  
+  // Far the wheel is turned 
+  var w = evt.wheelDelta;
+  console.log('scrolling');
+  console.log(w);
+  var pos = document.documentElement.scrollTop || document.body.scrollTop;
+  console.log(pos);
+  if (pos <= 300 && w > 0) {
+    console.log('yes');
+    document.documentElement.scrollTo(0, 0);
+    document.documentElement.scrollTop = 0;
+  }
+
+  // Returning normalized value 
+  return w / 120;
+}
+constructor() {
+  document.querySelector("html").addEventListener("mousewheel", this.wheelDistance.bind(this));
+}
+
+// Adding event listener for some element 
 }
