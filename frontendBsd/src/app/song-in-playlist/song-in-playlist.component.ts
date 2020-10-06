2d4f125eed13b5bc7c4369073d6463afd0e4fcc7
bsd <bsd.odaya@gmail.com>
2020-10-07 00:57:44 +0300

    Update song-in-playlist.component.ts

--- frontendBsd/src/app/song-in-playlist/song-in-playlist.component.ts
+++ frontendBsd/src/app/song-in-playlist/song-in-playlist.component.ts
@@ -11,18 +11,34 @@ export class SongInPlaylistComponent implements OnInit {
   @Input() song: Song;
   isPlay: boolean = false;
   checked: boolean = true;
+  src: HTMLAudioElement;
 
-  constructor() { }
+  constructor() { 
+    }
 
   ngOnInit() {
+    localStorage.setItem('isPlay', 'false');
+    this.src = new Audio("../../assets/songs/" + this.song.file_location);
   }
 
   play(): void {
-    this.isPlay = true;
+    if (localStorage.getItem('isPlay') == 'false') {
+      if (localStorage.getItem('previousSong') != this.song.id.toString()) {
+        this.src.load();
+      }
+      this.isPlay = true;
+      this.src.play();
+      localStorage.setItem('isPlay', 'true');
+      localStorage.setItem('currentSong', this.song.id.toString());
+    }
   }
 
   pause(): void {
     this.isPlay = false;
+    this.src.pause();
+    localStorage.setItem('isPlay', 'false');
+    localStorage.setItem('previousSong', localStorage.getItem('currentSong'));
+    localStorage.setItem('currentSong', null);
   }
 
   toggle(): void {
