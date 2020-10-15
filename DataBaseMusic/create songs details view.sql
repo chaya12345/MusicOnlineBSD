ebd5178e86f5616dc98b1e5af5006917f0f8c7d7
bsd <bsd.odaya@gmail.com>
2020-10-15 04:38:55 +0300

    Update profill in header

--- DataBaseMusic/create songs details view.sql
+++ DataBaseMusic/create songs details view.sql
@@ -10,7 +10,7 @@ GO
 
 create view [dbo].[songsDetails]
 as
-select s1.id, s1.name, s1.file_location, s1.type, s1.date, s1.count_responses, 
+select s1.id, s1.name, s1.file_location, s1.type, s1.date,
 s2.name as'singerName' ,s1.count_like,s1.count_views, s1.albumId,s1.title,s1.subtitle,s1.image_location,s1.content
 from SongsTBL s1 join SingersTBL s2 on s1.singerId=s2.id
 GO
