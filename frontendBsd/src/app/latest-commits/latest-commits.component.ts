307070193506295961424dd8a4e03409728a3910
bsd <bsd.odaya@gmail.com>
2020-10-19 19:30:09 +0300

    Update

--- frontendBsd/src/app/latest-commits/latest-commits.component.ts
+++ frontendBsd/src/app/latest-commits/latest-commits.component.ts
@@ -13,9 +13,16 @@ export class LatestCommitsComponent implements OnInit {
   lastCommits: LastesResponsView[];
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   constructor(private latestResponsesService: LatestResponsesService,private router:Router) {
     try {
       this.latestResponsesService.getLastResponsesToArticlesAndSongs().subscribe(lastResponses =>{ this.lastCommits = lastResponses; }, err => console.log(err));
+=======
+  constructor(private resToArticlesService: ResponsesToArticlesService, private router: Router) {
+    try {
+      this.resToArticlesService.GetLastResponsesToArticlesAndSongs().subscribe(lastResponses => { this.lastCommits = lastResponses; }, err => console.log(err));
+
+>>>>>>> Stashed changes
 =======
   constructor(private resToArticlesService: ResponsesToArticlesService, private router: Router) {
     try {
@@ -29,16 +36,22 @@ export class LatestCommitsComponent implements OnInit {
   ngOnInit() {
   }
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   getResponse(commit:LastesResponsView){
     if(commit.type=='article')
     this.router.navigateByUrl("article/"+commit.topicId);
     else this.router.navigateByUrl("song/"+commit.topicId);
 =======
+=======
+>>>>>>> Stashed changes
   
   getResponse(commit: LastResponsView) {
     if (commit.type == 'article')
       this.router.navigateByUrl("article/" + commit.topicId);
     else this.router.navigateByUrl("song/" + commit.topicId);
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
   }
 
