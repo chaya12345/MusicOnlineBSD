abcfbfaf7f604339232a18aa53f04f304d3b4c91
bsd <bsd.odaya@gmail.com>
2020-10-20 03:37:54 +0300

    Create alter-latest-commits.sql

---
+++ DataBaseMusic/alter-latest-commits.sql
@@ -0,0 +1,21 @@
+USE [MusicOnline]
+GO
+
+/****** Object:  View [dbo].[latestResponses]    Script Date: 19/10/2020 20:21:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER VIEW [dbo].[latestResponses]
+AS
+select top 20 id, type, name, date, topic, topicId from (
+select r.id, 'song' AS type, r.name, r.date, s.title as topic, s.id as topicId
+from ResponsesToSongsTBL as r join SongsTBL as s on r.songId = s.id
+union
+select r.id, 'article' AS type, name, r.date, a.title as topic, a.id as topicId
+from ResponsesToArticlesTBL as r join ArticlesTBL as a on r.articleId = a.id) as X order by date desc
+GO
+
+
