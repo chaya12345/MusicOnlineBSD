a96d04a953000a9da22ba6e12fcd82617051c2bd
bsd <bsd.odaya@gmail.com>
2020-10-20 17:18:09 +0300

    Update response

--- frontendBsd/src/app/response/response.component.ts
+++ frontendBsd/src/app/response/response.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,6 +10,7 @@ export class ResponseComponent implements OnInit {
 
   @Input() response: any;
   id: number;
+  @ViewChild('someVar') el:ElementRef;
 
   constructor(public activatedRoute: ActivatedRoute) { }
 
@@ -17,12 +18,32 @@ export class ResponseComponent implements OnInit {
     if (this.activatedRoute.snapshot.paramMap.get('commitId') != (null || undefined)) {
       this.id = Number(this.activatedRoute.snapshot.paramMap.get('commitId'));
       if(this.response.id==this.id){
+        // let el = document.getElementsByTagName("mat-accordion");
+        // for (let i = 0; i < el.length; i++) {
+        //   (el[i] as HTMLElement).style.border = "solid 3px red !important";
+        // }
         //זה מה שנותן בקונסול
        // this.response = {id: 1044, songId: 71, name: "יאיר", title: "וווווווואו וואו וואו! ענקקקק!", content: "❤❤❤", …}
-
       }
     }
     //console.log(this.response.content == '');
   }
+  
+  ngAfterViewInit() {
+    // console.log(this.rd); 
+    console.log(this.el);
+    if (this.activatedRoute.snapshot.paramMap.get('commitId') != (null || undefined)) {
+      this.id = Number(this.activatedRoute.snapshot.paramMap.get('commitId'));
+      if(this.response.id==this.id) {
+        console.log("yes");
+        console.log(this.el);
+        console.log("----------");
+        this.el.nativeElement.scrollIntoView();
+        this.el.nativeElement.style.backgroundColor = "whitesmoke";
+        // .scrollIntoView();
+      }
+    }
+    this.el.nativeElement.focus();      //<<<=====same as oldest way
+}
 
 }
