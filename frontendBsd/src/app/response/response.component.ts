05c316d80d711248f86a77abbadae5b2cbfe31b9
bsd <bsd.odaya@gmail.com>
2020-10-20 17:19:33 +0300

    Update response

--- frontendBsd/src/app/response/response.component.ts
+++ frontendBsd/src/app/response/response.component.ts
@@ -18,19 +18,13 @@ export class ResponseComponent implements OnInit {
     if (this.activatedRoute.snapshot.paramMap.get('commitId') != (null || undefined)) {
       this.id = Number(this.activatedRoute.snapshot.paramMap.get('commitId'));
       if(this.response.id==this.id){
-        // let el = document.getElementsByTagName("mat-accordion");
-        // for (let i = 0; i < el.length; i++) {
-        //   (el[i] as HTMLElement).style.border = "solid 3px red !important";
-        // }
         //זה מה שנותן בקונסול
        // this.response = {id: 1044, songId: 71, name: "יאיר", title: "וווווווואו וואו וואו! ענקקקק!", content: "❤❤❤", …}
       }
     }
-    //console.log(this.response.content == '');
   }
   
   ngAfterViewInit() {
-    // console.log(this.rd); 
     console.log(this.el);
     if (this.activatedRoute.snapshot.paramMap.get('commitId') != (null || undefined)) {
       this.id = Number(this.activatedRoute.snapshot.paramMap.get('commitId'));
@@ -40,10 +34,8 @@ export class ResponseComponent implements OnInit {
         console.log("----------");
         this.el.nativeElement.scrollIntoView();
         this.el.nativeElement.style.backgroundColor = "whitesmoke";
-        // .scrollIntoView();
       }
     }
-    this.el.nativeElement.focus();      //<<<=====same as oldest way
-}
+  }
 
 }
