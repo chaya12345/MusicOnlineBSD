bcdd92caa4b5351e17de65561f3bbf45d03b1313
bsd <bsd.odaya@gmail.com>
2020-10-20 21:29:55 +0300

    Create itemsBySinger.sql

---
+++ DataBaseMusic/itemsBySinger.sql
@@ -0,0 +1,40 @@
+CREATE FUNCTION itemsBySinger (@singer varchar(50))
+RETURNS table 
+AS
+RETURN 
+  (
+	select so.id, title, subtitle, date, null as dateHebrew, image_location as image,
+CASE WHEN isPerformance = 'true' THEN 'performance'
+WHEN EXISTS (SELECT * FROM SongsTBL as song join TagsToSongsTBL as tagts
+on song.id = tagts.songId join TagsTBL as tag on tag.id = tagts.tagId
+Where song.id = so.id and tag.name = 'קליפ') THEN 'clip'
+ELSE 'song' END AS type
+from SongsTBL as so join SingersTBL as si
+on so.singerId = si.id where si.name = @singer
+union
+select so.id, title, subtitle, date, null as dateHebrew, image_location as image,
+CASE WHEN isPerformance = 'true' THEN 'performance'
+WHEN EXISTS (SELECT * FROM SongsTBL as song join TagsToSongsTBL as tagts
+on song.id = tagts.songId join TagsTBL as tag on tag.id = tagts.tagId
+Where song.id = so.id and tag.name = 'קליפ') THEN 'clip'
+ELSE 'song' END AS type
+from SongsTBL as so join TagsToSongsTBL as tts
+on so.id = tts.songId join TagsTBL as t on tts.tagId = t.id where t.name = @singer
+union
+select so.id, title, subtitle, date, null as dateHebrew, image_location as image,
+CASE WHEN isPerformance = 'true' THEN 'performance'
+WHEN EXISTS (SELECT * FROM SongsTBL as song join TagsToSongsTBL as tagts
+on song.id = tagts.songId join TagsTBL as tag on tag.id = tagts.tagId
+Where song.id = so.id and tag.name = 'קליפ') THEN 'clip'
+ELSE 'song' END AS type
+from SongsTBL as so join ArtistsToSongsTBL as ats
+on so.id = ats.songId join ArtistsTBL as a on ats.artistId = a.id where a.name = @singer
+union
+select a.id, title, subtitle, date, dateHebrew, main_image as image, 'article' as type
+from ArticlesTBL as a
+join TagsToArticlesTBL as tta on a.id = tta.articleId join TagsTBL as t on tta.tagId = t.id 
+	where t.name = @singer
+  )
+GO
+
+select * from itemsBySinger('איציק דדיה') order by date desc
