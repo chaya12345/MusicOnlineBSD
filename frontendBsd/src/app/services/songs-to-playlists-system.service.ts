324bcde620c504a977a42d2718ee12063ac47d2a
Hodaya-Azran <0504117455h@gmail.com>
2020-11-09 17:38:39 +0200

    Update songs-to-playlists-system.service.ts

--- frontendBsd/src/app/services/songs-to-playlists-system.service.ts
+++ frontendBsd/src/app/services/songs-to-playlists-system.service.ts
@@ -9,7 +9,7 @@ import { SongsToPlaylistsSystem } from '../classes/songsToPlaylistsSystem';
 })
 export class SongsToPlaylistsSystemService {
 
-  baseUrl: string = "https://localhost:44368/api/SongsToPlaylostsSystem";
+  baseUrl: string = "https://localhost:44368/api/SongsToPlaylistsSystem";
   constructor(private httpClient: HttpClient) { }
 
   getSongsToPlaylistSystem(plalistId: number): Observable<Song[]> {
