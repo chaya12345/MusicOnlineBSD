30cb23f29b0b7ffb690a7f5255e9fca05d12dd43
bsd <bsd.odaya@gmail.com>
2020-11-09 16:51:02 +0200

    Update SongsToPlaylistsSystemController.cs

--- ServerMusicBSD/ServerMusicBSD/Controllers/SongsToPlaylistsSystemController.cs
+++ ServerMusicBSD/ServerMusicBSD/Controllers/SongsToPlaylistsSystemController.cs
@@ -10,5 +10,21 @@ namespace ServerMusicBSD.Controllers
 {
     public class SongsToPlaylistsSystemController
     {
+        public List<songsDetails> GetSongsToPlaylistSystem(int playlistId)
+        {
+            return SongsToPlaylistsSystemBL.GetSongsToPlaylistSystem(playlistId);
+        }
+        public void PostSongToPlaylistSystem(SongsToPlaylistsSystemTBL stp)
+        {
+            SongsToPlaylistsSystemBL.AddSongToPlaylistSystem(stp);
+        }
+        public void PostSongsToPlaylistSystem(List<SongsToPlaylistsSystemTBL> stpList)
+        {
+            SongsToPlaylistsSystemBL.AddSongsToPlaylistSystem(stpList);
+        }
+        public void DeleteSongFromPlaylistSystem(int playlistId, int songId)
+        {
+            SongsToPlaylistsSystemBL.DeleteSongFromPlaylistSystem(playlistId, songId);
+        }
     }
 }
