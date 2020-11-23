478c7ee3c3d115f33f4985677d6a1544e0e85724
bsd <bsd.odaya@gmail.com>
2020-11-23 22:27:31 +0200

    Update ParadeBL.cs

--- ServerMusicBSD/BL/ParadeBL.cs
+++ ServerMusicBSD/BL/ParadeBL.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
 namespace BL
 {
-    class ParadeBL
+    public class ParadeBL
     {
+        public static ParadeTBL GetActiveParade()
+        {
+            MusicOnlineEntities et = new MusicOnlineEntities();
+            return et.ParadeTBL.Where(p => p != null && p.dateEnd != null).FirstOrDefault();
+        }
+        public static ParadeTBL GetParadeByYear(string year)
+        {
+            MusicOnlineEntities et = new MusicOnlineEntities();
+            return et.ParadeTBL.Where(p => p != null && p.year == year).FirstOrDefault();
+        }
+        public static bool AddParade(ParadeTBL parade)
+        {
+            if (parade == null)
+                return false;
+            MusicOnlineEntities et = new MusicOnlineEntities();
+            try
+            {
+                et.ParadeTBL.Add(parade);
+                et.SaveChanges();
+                return true;
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        System.Console.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+                return false;
+            }
+        }
     }
 }
