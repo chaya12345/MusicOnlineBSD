b4c17e187c79cf5beed329d7c6a790a302c81189
bsd <bsd.odaya@gmail.com>
2020-11-23 21:49:27 +0200

    Update parade

---
+++ ServerMusicBSD/DTO/ParadeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ParadeDTO
+    {
+        public int id { get; set; }
+        public string year { get; set; }
+        public Nullable<System.DateTime> dateStart { get; set; }
+        public Nullable<System.DateTime> dateEnd { get; set; }
+        public string image { get; set; }
+    }
+}
---
+++ ServerMusicBSD/ServerMusicBSD/Controllers/SingersToParadeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using DAL;
+using BL;
+using DTO;
+
+namespace ServerMusicBSD.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class SingersToParadeController : ApiController
+    {
+        public List<SingersDTO> GetSingersInParade()
+        {
+            return SingersToParadeBL.GetSingersInParade();
+        }
+        public void PutVotingToSinger(int singerId)
+        {
+            SingersToParadeBL.AddVotingToSinger(singerId);
+        }
+        public void PostSingerToParade([FromBody] SingersToParadeTBL parade)
+        {
+            SingersToParadeBL.AddSingerToParade(parade);
+        }
+        public void PostSingersToParade([FromBody] List<SingersToParadeTBL> parades)
+        {
+            SingersToParadeBL.AddSingersToParade(parades);
+        }
+        public void DeleteSingerFromParade(int singerId)
+        {
+            SingersToParadeBL.DeleteSingerFromParade(singerId);
+        }
+    }
+}
