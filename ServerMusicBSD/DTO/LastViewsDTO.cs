ea799c8f8b7bab7b5a7a1f6bed8982309de8a92b
bsd <bsd.odaya@gmail.com>
2020-12-02 18:20:21 +0200

    Update

---
+++ ServerMusicBSD/DTO/LastViewsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class LastViewsDTO
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string subtitle { get; set; }
+        public Nullable<System.DateTime> date { get; set; }
+        public Nullable<System.DateTime> lastViewingDate { get; set; }
+        public string type { get; set; }
+    }
+}
