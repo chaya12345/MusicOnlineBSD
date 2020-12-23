eab950b20eb928c20cadc3e5514cdfe5987abe49
Hodaya-Azran <0504117455h@gmail.com>
2020-12-23 22:56:01 +0200

    Update uploading-song.component.ts

--- frontendBsd/src/app/uploading-song/uploading-song.component.ts
+++ frontendBsd/src/app/uploading-song/uploading-song.component.ts
@@ -124,7 +124,6 @@ export class UploadingSongComponent implements OnInit {
 
   onSubmit(): void {
     this.uploadSong.controls.song.setValue("songs/");
-    this.mailDetails = this.openDialogToMailDetails();
     if (this.uploadSong.valid && this.imageFile != null && this.songFile != null) {
       let folderOfSinger = this.convertToFolderName(this.uploadSong.controls.singers.value[0]);
       this.uploadSong.controls.image.setValue("for_songs/" + folderOfSinger + "/" + this.imageFile.name);
