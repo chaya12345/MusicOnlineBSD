f9b4347a91a8293809c9d0b7ba52818756314d56
bsd <bsd.odaya@gmail.com>
2021-01-03 18:57:22 +0200

    uploading editing content management area in admin area  only bsd!

--- frontendBsd/src/app/content-management/content-management.component.ts
+++ frontendBsd/src/app/content-management/content-management.component.ts
@@ -23,11 +23,44 @@ export class ContentManagementComponent implements OnInit {
   scrollToElement($element): void {
     console.log($element);
     // this.playlist.nativeElement.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    let state = window.pageYOffset;
+    window.scrollTo(0, state += 500);
     $element.scrollIntoView({ behavior: "smooth", block: "start" });
     // const elementRect = $element.getBoundingClientRect();
     // const absoluteElementTop = elementRect.top + window.pageYOffset;
     // const middle = absoluteElementTop - (elementRect.height / 2);  
     // window.scrollTo(0, middle - 200);
+
+    // let state = window.pageYOffset;
+    // let scrolling = setInterval(() => {
+    //   if (this.elementInViewport($element) == false) {
+    //     window.scrollTo(0, state += 10);
+    //   }
+    //   else {
+    //     clearInterval(scrolling);
+    //   }
+    // }, 100);
+
+  }
+
+  elementInViewport(el): boolean {
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
+
+    while (el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
+    }
+
+    return (
+      top >= window.pageYOffset &&
+      left >= window.pageXOffset &&
+      (top + height) <= (window.pageYOffset + window.innerHeight) &&
+      (left + width) <= (window.pageXOffset + window.innerWidth)
+    );
   }
 
 }
